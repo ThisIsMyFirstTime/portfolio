5d33764bc8e0dea4083c55e18d86de6fa6810fb7
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2023-02-08 00:51:15 -0600

    added pillow package for image and started building an icon gatherer

--- weatherapp/api_connection.py
+++ weatherapp/api_connection.py
@@ -1,21 +1,40 @@
 import requests
 
-
 class WeatherAPI:
     def __init__(self, api_key, location):
         self.url = 'https://api.weatherapi.com/v1/current.json?key=' + str(api_key) + "&q=" + str(location)
 
+    @property
     def response(self):
         return requests.get(self.url)
 
+    @property
     def to_dict(self):
-        return self.response().json()
+        return self.response.json()
+
+    @property
+    def current_temp(self):
+        return self.to_dict['current']['temp_f']
+
+    @property
+    def current_condition(self):
+        return self.to_dict['current']['condition']['text']
+
+    @property
+    def current_condition_icon(self):
+        return self.to_dict['current']['condition']['icon']
+
+    def image_grab(self):
+        url = "https:" + self.current_condition_icon
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open('weather.json', 'wb') as f:
+                f.write(response.content)
 
 
-
-
+class NotScriptError(Exception):
+    pass
 
 
 if __name__ == '__main__':
-    weather = WeatherAPI()
-    print(weather.response)
+    raise NotScriptError('This is not a script.')
--- weatherapp/app.py
+++ weatherapp/app.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     API_KEY = 'c077415b0bf4406b9fd223106230702'
     location = '76017'
     weather = WeatherAPI(API_KEY, location)
-    print(weather.to_dict()['current']['condition'])
+    print(weather.to_dict['current']['condition'])
+    weather.current_condition_icon
+    weather.image_grab()
+
 
